e948369f135b8df84feb803168fb5a4f3cced36a
Brandon-Skarka <107329282+Brandon-Skarka@users.noreply.github.com>
2022-12-01 03:45:50 -0800

    Added Pins.hpp for defining all of the arduino pins used

---
+++ Navigation/src/Pins.hpp
@@ -0,0 +1,32 @@
+#ifndef Pins_h
+#define Pins_h
+
+// Drivetrain -----
+// Front Bridge
+#define FRONTENA 5
+#define FRONTIN1 31
+#define FRONTIN2 30
+
+#define FRONTENB 6
+#define FRONTIN3 28
+#define FRONTIN4 29
+
+
+// Back Bridge
+#define BACKENA 10
+#define BACKIN1 37
+#define BACKIN2 36
+
+#define BACKENB 9
+#define BACKIN3 34
+#define BACKIN4 35
+
+// ULtrasonic -----
+
+
+// Mechansim -----
+#define CLAWPIN 2
+#define TILTPIN 3
+
+
+#endif
